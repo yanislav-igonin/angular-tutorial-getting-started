9e4f59a4fdc5afc5bd70117fa508b8e5f518b24e
yanislav-igonin <yanislavdabest@gmail.com>
2021-10-19 11:06:24 +0300

    feat: add shipping page

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-shipping',
   templateUrl: './shipping.component.html',
   styleUrls: ['./shipping.component.css'],
 })
-export class ShippingComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
+export class ShippingComponent {
+  shippingCosts = this.cartService.getShippingPrices();
+  
+  constructor(private cartService: CartService) {}
 }
