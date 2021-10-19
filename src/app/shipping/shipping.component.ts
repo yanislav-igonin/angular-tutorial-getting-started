5bf69bc4df88297802cfaccb548a0143a1412f00
yanislav-igonin <yanislavdabest@gmail.com>
2021-10-19 10:41:57 +0300

    feat: add basic shipping component

---
+++ src/app/shipping/shipping.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shipping',
+  templateUrl: './shipping.component.html',
+  styleUrls: ['./shipping.component.css'],
+})
+export class ShippingComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
